494c287a3b4cfaf107cfbf42013a7106d7a37e29
Yulya Krilova <yulyakrilova@gmail.com>
2022-03-30 17:37:08 +0300

    new commit

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log('Привет, мир')
+
+let entrance = 4;
+let floors = 9;
+let flatsperfloor = 4;
+let flatsperentrance = floors * flatsperfloor
+console.log('Квартир в подъезде', flatsperentrance)
+let flats = flatsperentrance * entrance
+console.log('Всего квартир', flats)
